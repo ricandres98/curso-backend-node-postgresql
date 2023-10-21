b8b834ea950f04ae2318d0fb871c7660ffa41dd5
Ricardo <ricardo270298@gmail.com>
2023-10-21 14:27:21 -0400

    FIX Leyendo URI para Sequelize de variables de entorno

--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || "dev",
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
+  dbUrl: process.env.DB_URL
 }
 
 module.exports = { config };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,7 +5,10 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI =
+  config.env === 'production'
+    ? config.dbUrl
+    : `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
