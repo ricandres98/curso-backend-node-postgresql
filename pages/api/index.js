3b6b82a8b8820beeba88978a004e40d554d65cf4
Ricardo <ricardo270298@gmail.com>
2023-10-21 14:11:05 -0400

    lint fix

--- pages/api/index.js
+++ pages/api/index.js
@@ -11,16 +11,16 @@ const port = config.port || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  }
-}
+// const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'));
+//     }
+//   }
+// }
 app.use(cors());
 
 app.get('/api', (req, res) => {
