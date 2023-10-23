cefa9eac3b591083c5ae161ee97063ca4bc09382
Ricardo <ricardo270298@gmail.com>
2023-10-22 21:08:07 -0400

    db/config.js reads dinamically database URI from enviroment

--- db/config.js
+++ db/config.js
@@ -2,8 +2,10 @@ const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+const URI =
+  config.env === 'production'
+    ? config.dbUrl
+    : `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
